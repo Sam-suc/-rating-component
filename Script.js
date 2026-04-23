2a851ebf4a52386f44e16425732a740f112c42a0
Sami <sami.work2130@Gmail.com>
2026-04-23 00:54:49 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,17 +1,17 @@
 
-    // Inside your first JS file or <script> tag:
+   
    const submitBtn = document.querySelector('.submit-btn');
 
    submitBtn.addEventListener('click', () => {
-    // 1. Get the checked radio button
+    //  Get the checked radio button
     const selected = document.querySelector('input[name="rating"]:checked');
     
     if (selected) {
-        // 2. SAVE the value to localStorage
+        //  SAVE the value to localStorage
         localStorage.setItem('userRating', selected.value);
         
         
-        // 3. Move to the next page
+        //  Move to the next page
         window.location.href = "new.html"; 
     } else {
       alert("Please select a rating!")
